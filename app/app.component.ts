5b14be99d367cda2d439b74c89ff679cbc7bd64d
Nav Patel <patelab@gmail.com>
2016-12-08 00:18:31 -0500

    If you include moduleId “app.component.css” works!

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
   template: `
       <h1>Helloooo {{name}}</h1>
       <my-detail></my-detail>
   `,
-  styleUrls:['app/app.component.css']
+  styleUrls:['app.component.css']
 })
 export class AppComponent  { name = 'Angular'; }
